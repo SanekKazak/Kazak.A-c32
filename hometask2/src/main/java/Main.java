4ace9d936484a4a178afd9a28c305c53a14490d0
SanekKazak <kazaksanek2003@gmail.com>
2025-02-16 19:59:29 +0300

    Corrected hometask 2

--- hometask2/src/main/java/Main.java
+++ hometask2/src/main/java/Main.java
@@ -1,40 +1,38 @@
 import java.util.Random;
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 public class Main {
     public static void main(String[] args) {
         //TASK1 - Дано произвольное число. Необходимо узнать, сколько в конце числа нулей.
         //realization #1 - подсчёт количества нулей в числе
         Random random = new Random();
-        long valueTask1 = random.nextLong(1000000);
-        for(int i=0;;){
-            if(valueTask1 < 10){
-                System.out.println(i);
-                break;
-            }
+        long valueTask1 = random.nextLong();
+        int counter = 0;
+        while(valueTask1 > 0){
             valueTask1 /= 10;
-            i++;
+            counter++;
         }
+        System.out.println(counter);
 
         //realization #2 - подсчёт количества нулей в конце числа
-        valueTask1 = random.nextLong(1000000);
-        for(int i = 0;;){
-            if(valueTask1%10==0){
-                i++;
-                valueTask1 /= 10;
-            } else {
-                System.out.println(i);
-                break;
+        valueTask1 = random.nextLong();
+        counter = 0;
+        while(valueTask1 > 0){
+            if(valueTask1 % 10 == 0){
+                counter++;
             }
+            valueTask1 /= 10;
         }
+        System.out.println(counter);
 
         //TASK2 считаем интеграл n
         Scanner scanner = new Scanner(System.in);
         int valueTask2 = scanner.nextInt();
         int integral = 1;
-        while(valueTask2 > 0){
-            integral *= valueTask2;
-            valueTask2--;
+        for (int i = 1; i <= valueTask2; i++) {
+            integral *= i;
         }
         System.out.println(integral);
 
@@ -42,6 +40,10 @@ public class Main {
         // Необходимо найти среднее арифметическое между заданными числами
         int valueTask3_1 = scanner.nextInt();
         int valueTask3_2 = scanner.nextInt();
-        System.out.println((double)(valueTask3_2+valueTask3_1)/2);
+        int value = 0;
+        for(int i = valueTask3_1; i < valueTask3_2+1; i++ ){
+            value += i;
+        }
+        System.out.println(value/(abs(valueTask3_1-valueTask3_2)+1));
     }
 }
