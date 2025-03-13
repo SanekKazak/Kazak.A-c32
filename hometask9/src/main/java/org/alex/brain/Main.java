4d8444cffcac49d8d536948ff9228c7b8a69cf4f
Alex <kazaksanek2003@gmail.com>
2025-03-13 12:45:32 +0300

    hometask9

--- hometask9/src/main/java/org/alex/brain/Main.java
+++ hometask9/src/main/java/org/alex/brain/Main.java
@@ -6,23 +6,26 @@ import org.alex.brain.service.EmployeeService;
 
 public class Main {
     public static void main(String[] args) {
-        Director director = new Director("Alex", "Braun", 2);
-        Director director1 = new Director("Alexo", "Brauno", 20);
+        Director director0 = new Director("Alex", "Braun", 2);
+        Director director1 = new Director("Ale", "Brau", 10);
+        Director director2 = new Director("Lex", "Bra", 24);
+        Director director3 = new Director("Alexo", "Br", 15);
         Worker worker1 = new Worker("Domenic", "Elis", 3);
         Worker worker2 = new Worker("Donny", "Frun", 1);
         Worker worker3 = new Worker("Tom", "Loya", 22);
         Worker worker4 = new Worker("Pad", "Noi", 7);
-        System.out.println(worker2);
-        System.out.println(worker3);
-        System.out.println(worker4);
-        System.out.println(director);
-        director.add(worker1);
-        System.out.println(director);
-        director1.add(director);
-        System.out.println(director1);
+        director0.add(worker1);
+        director1.add(worker2);
+        director2.add(worker3);
+        director3.add(worker4);
+        director0.add(director1);
+        director1.add(director2);
+        director2.add(director3);
         EmployeeService employeeService = new EmployeeService();
-        employeeService.getCheckResult(director1, worker1);
-        employeeService.getCheckResult(director1, worker2);
-        employeeService.getCheckResult(director1, director);
+        System.out.println(employeeService.getCheckResult(director0, director3));
+        System.out.println(employeeService.getCheckResult(director0, worker4));
+        System.out.println(employeeService.getCheckResult(director1, director0));
+
+
     }
 }
--- hometask9/src/main/java/org/alex/brain/service/EmployeeService.java
+++ hometask9/src/main/java/org/alex/brain/service/EmployeeService.java
@@ -2,14 +2,26 @@ package org.alex.brain.service;
 
 import org.alex.brain.abstracts.classes.Employee;
 import org.alex.brain.employee.Director;
+import org.alex.brain.enums.Role;
 
 public class EmployeeService {
-    public void getCheckResult(Director director, Employee employee){
+/*
+    В 19 строке это условие:
+    ...!employeeRunner.equals(director)...
+    используется из-за метода добавления самого директора
+    в каждом списке подчинённых у директоров - EmployeeArray
+    этот же директор идёт первым, следовательно для того,
+    чтобы рекурсия работала нужно пропускать его
+    */
+    public boolean getCheckResult(Director director, Employee employee){
         for(Employee employeeRunner : director.getEmployeeArray()){
             if(employeeRunner.getName().equals(employee.getName())){
-                System.out.println("Congratulations");
-                break;
+                return true;
+            }
+            if(employeeRunner.getRole().equals(Role.DIRECTOR) && !employeeRunner.equals(director)){
+                return getCheckResult((Director)employeeRunner, employee);
             }
         }
+        return false;
     }
 }
