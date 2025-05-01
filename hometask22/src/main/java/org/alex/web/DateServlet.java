fefaea88f7ab3d720bb3f6934bcc689fa1526573
SanekKazak <kazaksanek2003@gmail.com>
2025-05-01 19:02:58 +0300

    hometask22

--- hometask22/src/main/java/org/alex/web/DateServlet.java
+++ hometask22/src/main/java/org/alex/web/DateServlet.java
@@ -7,7 +7,16 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
+/*
+создать проект, поделючить библиотеку servlet-api
+и подключить плагин war
 
+создать сервлет, прописать его маппинг
+и в самом сервлете вывести в ответ - текущую дату и время
+
+проверить что браузер может открыть страницу
++ так же проверить работу через postman
+ */
 public class DateServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
