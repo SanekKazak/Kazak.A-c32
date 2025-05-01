1153bfb5c24b3e822f01ecc8c7acefb501a4d743
SanekKazak <kazaksanek2003@gmail.com>
2025-05-01 15:54:07 +0300

    hometask22

---
+++ hometask22/src/main/java/org/alex/web/DateServlet.java
@@ -0,0 +1,18 @@
+package org.alex.web;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+public class DateServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("Not works");
+        PrintWriter writer = resp.getWriter();
+        writer.println("time now " + LocalDateTime.now());
+    }
+}
